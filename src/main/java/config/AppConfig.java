41c8a6c9af5f07598078045cb905b3f1dc357dcc
Анатолий Андреев <anatoliiandreev19@gmail.com>
2022-07-26 02:04:47 +0300

    25.07

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -11,6 +11,13 @@ import javax.sql.DataSource;
 import java.util.Objects;
 import java.util.Properties;
 
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @PropertySource("classpath:db.properties")
 @EnableTransactionManagement
